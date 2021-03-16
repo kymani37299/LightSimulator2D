3f361e1c2280bda2ec941dfe226cca9a0c3c98f2
Marko Sreckovic <markosrecko97@hotmail.rs>
2021-03-16 15:04:29 +0100

    Forest scene #1

--- LightSimulator2D/src/scene/DemoScene.cpp
+++ LightSimulator2D/src/scene/DemoScene.cpp
@@ -7,6 +7,7 @@
 #include "scene/components/FollowMouseComponent.h"
 
 static void Scene1(Scene* scene, PlayerControllerComponent* controller);
+static void Scene2(Scene* scene, PlayerControllerComponent* controller);
 
 PlayerControllerComponent* Demo::SetupDemoScene(Scene* scene, unsigned index)
 {
@@ -15,6 +16,7 @@ PlayerControllerComponent* Demo::SetupDemoScene(Scene* scene, unsigned index)
     switch (index)
     {
     case 1: Scene1(scene, controller); break;
+    case 2: Scene2(scene, controller); break;
     default: Scene1(scene, controller); break;
     }
 
@@ -29,6 +31,7 @@ static void Scene1(Scene* scene, PlayerControllerComponent* controller)
 
     Entity* e1 = new Entity{ "res/animals/elephant.png", "res/animals/elephant_normal3.jpg" };
     e1->GetDrawFlags().occluder = true;
+    e1->GetDrawFlags().foreground = true;
     e1->GetOcclusionProperties().shape = OccluderShape::Mesh;
     e1->GetOcclusionProperties().meshLod = 2;
     e1->m_Transform.rotation = 3.1415f / 4.0f;
@@ -57,4 +60,38 @@ static void Scene1(Scene* scene, PlayerControllerComponent* controller)
     scene->AddEntity(e2);
     scene->AddEntity(e3);
     scene->AddEntity(e4);
+}
+
+static void Scene2(Scene* scene, PlayerControllerComponent* controller)
+{
+    const std::string res_path = "res/scene2/";
+
+#define P(X) res_path + X
+
+    Entity* bg = new Entity{P("bg_diffuse.png"), P("bg_normal.png") };
+    bg->GetDrawFlags().background = true;
+    bg->GetBackgroundProperties().textureScale = 2.0f;
+
+    Entity* emitter = new Entity{ P("bg_diffuse.png"), P("bg_normal.png") };
+    emitter->AddComponent(new FollowMouseComponent());
+    emitter->m_Transform.scale *= 0.01;
+    emitter->GetDrawFlags().emitter = true;
+    emitter->GetEmissionProperties().color = Vec3(1.0, 1.0, 0.0);
+    emitter->GetEmissionProperties().radius = 0.15f;
+
+    Entity* tree_up = new Entity{ P("tree1_up.png") };
+    tree_up->GetDrawFlags().foreground = true;
+    tree_up->AddComponent(controller);
+
+    Entity* tree_down = new Entity{P("tree1_down.png")};
+    tree_down->GetDrawFlags().occluder = true;
+    tree_down->m_Transform.position = tree_up->m_Transform.position + Vec2(0.01,-0.2);
+    tree_down->GetOcclusionProperties().shape = OccluderShape::Rect;
+
+#undef P
+
+    scene->AddEntity(bg);
+    scene->AddEntity(emitter);
+    scene->AddEntity(tree_up);
+    scene->AddEntity(tree_down);
 }
